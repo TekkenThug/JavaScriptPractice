c6d460664d0a637eea05eeca333035779d98df7e
Vadim Ignatov <ignatov0131@gmail.com>
2021-01-08 14:33:03 +0300

    Валидация ответов пользователя

--- MoviesJS/script.js
+++ MoviesJS/script.js
@@ -12,7 +12,11 @@ for (let i = 0; i < 2; i++) {
   const a = prompt("Один из последних просмотренных фильмов?", ""),
         b = prompt("На сколько вы оцените его?", "");
 
-  personalMovieDB.movies[a] = b; 
+  if (a != null && a.length < 50 && a != "" && b != null && b != "") {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
 }
 
 console.log(personalMovieDB);
