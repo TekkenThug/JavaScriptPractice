05c8737efe7012fbf570a575d8c3a5779be77188
Vadim Ignatov <ignatov0131@gmail.com>
2021-01-08 13:37:17 +0300

    Добавление персональной БД фильмов пользователя

--- MoviesJS/script.js
+++ MoviesJS/script.js
@@ -1,1 +1,9 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
