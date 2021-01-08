b66550d0ddee1b9beea73c12ee45d1f42bc9ba59
Vadim Ignatov <ignatov0131@gmail.com>
2021-01-08 13:45:38 +0300

    Запись ответов пользователя

--- MoviesJS/script.js
+++ MoviesJS/script.js
@@ -7,3 +7,13 @@ let personalMovieDB = {
   genres: [],
   privat: false
 };
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько вы оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько вы оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
