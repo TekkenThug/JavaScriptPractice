10ecea43207cab454571e5cacfd61112867242b3
Vadim Ignatov <ignatov0131@gmail.com>
2021-01-12 11:40:11 +0300

    Сделанные табы

---
+++ Food/js/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+  
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show');
+    });
+
+    tabs.forEach(tab => {
+      tab.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(index = 0) {
+    tabsContent[index].classList.remove('hide');
+    tabsContent[index].classList.add('show');
+    tabs[index].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, index) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+});
