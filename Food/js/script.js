6f253bf8c2a091b4895b0e44b08d08aa903a292f
Vadim-Matvei-Notebook <ignatov0131@gmail.com>
2021-03-20 11:55:10 +0300

    Добавление калькулятора

--- Food/js/script.js
+++ Food/js/script.js
@@ -369,6 +369,77 @@ document.addEventListener("DOMContentLoaded", () => {
       updateCounterSlider(slideIndex, counterSlide);
       refreshDots(slideIndex);
     });
-  })
+  });
 
+
+  // Calculator
+  const totalCalories = document.querySelector(".calculating__result span");
+  
+  let gender = "female", height, weight, age, ratio = 1.375;
+
+  calcTotal();
+  getStaticData("#gender", "calculating__choose-item_active");
+  getStaticData("#activity", "calculating__choose-item_active");
+  getDynamicData("#height");
+  getDynamicData("#weight");
+  getDynamicData("#age");
+
+  function calcTotal() {
+    if (!gender || !height || !weight || !age || !ratio) {
+      totalCalories.textContent = "___";
+      return;
+    }
+
+    if (gender == "male") {
+      totalCalories.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+
+    } else if (gender == "female") {
+      totalCalories.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+    }
+  }
+
+  function getStaticData(parentSelector, activeClass) {
+    const elements = document.querySelectorAll(`${parentSelector} div`);
+
+    elements.forEach(elem => {
+      elem.addEventListener("click", (event) => {
+        if (event.target.getAttribute("data-ratio")) {
+          ratio = +event.target.getAttribute("data-ratio");
+        } else {
+          gender = event.target.getAttribute("id");
+        }
+  
+        elements.forEach(elem => {
+          elem.classList.remove(activeClass);
+        });
+  
+        event.target.classList.add(activeClass);
+  
+        calcTotal();
+      })
+    })
+
+  }
+
+  function getDynamicData(selector) {
+    const input = document.querySelector(selector);
+
+    input.addEventListener("input", () => {
+      switch(input.getAttribute("id")) {
+        case "height":
+          height = input.value;
+          break;
+        
+        case "weight":
+          weight = input.value;
+          break;
+        
+        case "age":
+          age = input.value;
+          break;
+      }
+
+      calcTotal();
+    });
+  }
 });
